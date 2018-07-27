9b5936d0320a2762856a39d6618fd0e6b4328f00
sussie7123@live.com <sussie7123@live.com>
2018-07-27 17:59:49 +0800

    Upload the files for HTML and CSS learning.

---
+++ 1.js
@@ -0,0 +1,6 @@
+function a () {
+    A=prompt("Please enter the 1st string");
+    B=prompt("Please enter the 2nd string");
+    document.write("The 1st String you entered is "+A);
+    document.write("The 2nd String you entered is "+B);
+}
